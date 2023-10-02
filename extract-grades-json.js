87850d8f8f7caccdbc0a2656a0249e382bc585a4
BragdonD <thomasduclos1012@gmail.com>
2023-10-02 08:27:19 +0200

    feat: cleanup the table to improve row picking

--- extract-grades-json.js
+++ extract-grades-json.js
@@ -78,6 +78,9 @@
   const yearRowClass = "master";
   const semesterAndModuleRowClass = "item-ens";
   const semesterStr = "Semestre";
+  const semesterMistakeStr = "Semestre Académique";
+  const moduleStr = ["Module", "LFH"];
+  const moduleMistakeStr = ["Module Mineure"];
   const courseRowClass = "item-fpc";
   const coursePartClass = "item-ev1";
   const nameColumnClass = "libelle";
@@ -85,6 +88,7 @@
   const gradeColumnClass = "note";
   const courseCoefficientClass = "ponderation";
   const weightColumnClass = "coefficient"; // they inverse it ...
+  const semesterNumber = 2;
 
   const resultatsContainer = document.querySelector(
     "#".concat(resultatsContainerId)
@@ -103,6 +107,49 @@
     const grades = str.split(" - ");
   };
 
+  const extractModuleNumber = (semesterRows) => {
+    const rows = Array.from(semesterRows);
+    let modulesRows = rows.filter((row) =>
+      row.querySelector(".".concat(semesterAndModuleRowClass))
+    );
+    modulesRows = modulesRows.filter((row) => {
+      for (let i = 0; i < moduleStr.length; i++) {
+        if (row.innerText.includes(moduleStr[i])) {
+          return true;
+        }
+      }
+      return false;
+    });
+    return modulesRows.length;
+  }
+
+  const extractModule = (semesterRows, i) => {
+    const rows = Array.from(semesterRows);
+    let modulesRows = rows.filter((row) =>
+      row.querySelector(".".concat(semesterAndModuleRowClass))
+    );
+    modulesRows = modulesRows.filter((row) => {
+      for (let i = 0; i < moduleStr.length; i++) {
+        if (row.innerText.includes(moduleStr[i])) {
+          return true;
+        }
+      }
+      return false;
+    });
+    const moduleRowIndex = rows.indexOf(modulesRows[i]);
+    let nextModuleRowIndex = rows.indexOf(modulesRows[i + 1]);
+    if (nextModuleRowIndex === -1) {
+      nextModuleRowIndex = rows.length;
+    }
+    const moduleRows = rows.slice(moduleRowIndex, nextModuleRowIndex);
+    console.log(moduleRows);
+    if (moduleRows.length === 0) {
+      return undefined;
+    } else {
+      return {}
+    }
+  };
+
   /**
    * Extract all the data for a
    * @param {Array<Element>} yearRows the rows of the table corresponding to the year
@@ -113,13 +160,23 @@
     const semestersAndModuleRows = rows.filter((row) =>
       row.querySelector(".".concat(semesterAndModuleRowClass))
     );
-    const semestersRows = semestersAndModuleRows.filter((row) =>
+    let semestersRows = semestersAndModuleRows.filter((row) =>
       row.innerText.includes(semesterStr)
     );
+    semestersRows = semestersRows.filter((row) =>
+      !row.innerText.includes(semesterMistakeStr)
+    );
     const semesterRowIndex = rows.indexOf(semestersRows[i]);
-    const nextSemesterRowIndex = rows.indexOf(semestersRows[i + 1]);
+    let nextSemesterRowIndex = rows.indexOf(semestersRows[i + 1]);
+    if (nextSemesterRowIndex === -1) {
+      nextSemesterRowIndex = rows.length;
+    }
     const semesterRows = rows.slice(semesterRowIndex, nextSemesterRowIndex);
-    console.log(semesterRows);
+    const modules = [];
+    for (let i = 0; i < extractModuleNumber(semesterRows); i++) {
+      const module = extractModule(semesterRows, i);
+      modules.push(module);
+    }
   };
 
   /**
@@ -135,17 +192,47 @@
     const yearRowIndex = rows.indexOf(yearsRows[i]);
     const nextYearRowIndex = rows.indexOf(yearsRows[i + 1]);
     const yearRows = rows.slice(yearRowIndex, nextYearRowIndex);
-    for (let i = 0; i < 2; i++) {
+    for (let i = 0; i < semesterNumber; i++) {
       extractSemester(yearRows, i);
     }
   };
 
+  /**
+   * This functions remove all the part that are stucks between two 
+   * .item-ens that are not a module or doesn't have a name...
+   * @param {HTMLAllCollection} tableRows the rows of the table
+   */
+  const removeUselessParts = (tableRows) => {
+    const rows = Array.from(tableRows);
+    const semestersAndModuleRows = rows.filter((row) =>
+      row.querySelector(".".concat(semesterAndModuleRowClass))
+    );
+    for(let i = 0; i < semestersAndModuleRows.length; i++) {
+      for(let j = 0; j < moduleStr.length; j++) {
+        if(semestersAndModuleRows[i].innerText.includes(moduleMistakeStr[j])) {
+          const rowIndex = rows.indexOf(semestersAndModuleRows[i]);
+          tableRows.item(rowIndex).remove();
+        }
+      }
+      if(semestersAndModuleRows[i].innerText === "") { // no name module that are a mistake
+        const rowIndex = rows.indexOf(semestersAndModuleRows[i]);
+        const nextRowIndex = rows.indexOf(semestersAndModuleRows[i + 1]);
+        for(let j = rowIndex; j < nextRowIndex; j++) {
+          tableRows.item(j).remove();
+        }
+      }
+    }
+  }
+
   resultatsContainer.arrive("#".concat(resultatsTableId), (table) => {
     const yearsCount = extractYearsCount(table);
     const years = [];
-    const tableRows = table.querySelectorAll("tr");
+    let tableRows = table.querySelectorAll("tr");
+    removeUselessParts(tableRows); // clean the table
+    tableRows = table.querySelectorAll("tr"); // update the tableRows
     for (let i = 0; i < yearsCount; i++) {
-      years.push(extractYear(tableRows, i));
+      const year = extractYear(tableRows, i);
+      years.push(year);
     }
   });
 })();
